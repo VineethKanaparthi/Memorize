0a09712ad190b67f8adc4b78ca13df61727d8f1b
vineethkanaparthi <vineeth0025@gmail.com>
2023-11-17 20:54:01 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Vineeth Kanaparthi on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
