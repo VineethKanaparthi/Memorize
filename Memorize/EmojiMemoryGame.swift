7c2d39d171a67accf4bd75e399614998871bae68
vineethkanaparthi <vineeth0025@gmail.com>
2023-11-18 17:04:30 -0700

    change to MVVM

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Vineeth Kanaparthi on 11/18/23.
+//
+
+import Foundation
---
+++ MemorizeGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by Vineeth Kanaparthi on 11/18/23.
+//
+
+import Foundation
